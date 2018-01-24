e2ab337bccd907228009f1ecf7a6888df1378194
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-01-24 17:18:29 +0100

    Add files via upload

---
+++ REST.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+<link rel="stylesheet" href=
+<script type="text/javascript" src=""></script>
+</head>
+<body>
+  <?php
+//fixed response
+//when GET request -> send info
+
+
+// when POST response -> store info
+
+
+
+//flatfile dataresponse
+
+//SQL Database
+   ?>
+
+</body>
+</html>
