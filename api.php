57359b8b2c139b4983ab42a51fded0d7bfea1e17
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-01-25 16:42:43 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<title></title>
+<link rel="stylesheet" href=
+<script type="text/javascript" src=""></script>
+</head>
+<body>
+
+
+<!-- fixed response when GET request Get messages-->
+Welcome <?php echo $_GET["name"]; ?><br>
+Your message is: <?php echo $_GET["message"]; ?>
+
+<!-- fixed response when POST request Create messages -->
+
+
+
+<!-- Tier 2 Flatfile datastructure -->
+
+
+<!-- Tier 3 SQL Database -->
+
+</body>
+</html>
